495b3a7c5a5202160c130336c142b5f27c167c71
DESKTOP-NUOPUIF\ITPS <yky2496@nave.com>
2021-03-15 17:19:05 +0900

    최초 프로젝트 업로드

---
+++ src/chapter1/Ex2.java
@@ -0,0 +1,40 @@
+package chapter1;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+	// 문자열- 문자의 나열
+	//문자열은 꼭 쌍따옴표를 사용해서 표현해야한다
+//	System.out.println("Hello World!");
+//	System.out.println("Hello");
+//	System.out.println("World~!");
+		
+// ystem.out.println("I am");
+// ystem.out.println("a");
+// ystem.out.println("java Programmer");
+	
+// ystem.out.print("오늘은");
+// ystem.out.print("자바 수업");
+// ystem.out.print("첫번쨰 날입니다");
+	
+//	System.out.print("I am ");
+//	System.out.print("a ");
+//	System.out.print("java programmer");
+
+//	System.out.println(1);
+	
+//	System.out.println(3.14);
+	
+//	System.out.println('a');
+	
+//	System.out.println("abcd");
+		
+	System.out.println("오늘의 기온 :" +-5.3 + "c");
+	System.out.println("커피 가격 :" +1500);
+	System.out.println("오늘의 기온 :" +-5.3 + "c");
+	
+	
+	
+	}
+
+}
